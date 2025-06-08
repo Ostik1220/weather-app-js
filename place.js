c608c5fa6d46f906f7ce3a51203cd799637b0b09
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-06-08 17:41:53 +0300

    save and add diferent styles

--- place.js
+++ place.js
@@ -2,10 +2,24 @@
 //  Використайте публічне API OpenWeatherMap для отримання поточної погоди.
 //  Зробіть GET-запит за адресою https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY},
 //  де {city} - назва міста, а {API_KEY} - ваш ключ API OpenWeatherMap. Перегляньте отримані дані щодо погоди.
+document.querySelector(".form").addEventListener("submit", (e) => {
+    e.preventDefault()
+    const city = document.querySelector("#input").value.toLowerCase()
+    console.log(city)
+    // if(data === undefined){
+    //     alert()
+    // }
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=Lviv&APPID=17901880cf618f6a938a98535f079158").then(response => response.json()).then(data => document.querySelector(".info").innerHTML = makeList(data))
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=17901880cf618f6a938a98535f079158`).then(response => response.json()).then(data => {
+    if(data.cod === "404"){
+        alert("Місто не знайдене")
+    } else {
+        document.querySelector(".info").innerHTML = makeList(data)
+    }
+})
 function makeList(obj){
-    const placeholder = `<ul class="weather-list">
+    const placeholder = `        <h1>Погода у ${city}</h1>
+    <ul class="weather-list">
             <li><p>Погода: ${obj.weather[0].main}</p></li>
             <li><p>Кількість хмар: ${obj.clouds.all}</p></li>
             <li><p>Вологість: ${obj.main.humidity} г/м^3</p></li>
@@ -22,5 +36,6 @@ function makeList(obj){
         </ul>`
         return placeholder
 }
-
+})
 fetch("https://api.openweathermap.org/data/2.5/weather?q=Lviv&APPID=17901880cf618f6a938a98535f079158").then(response => response.json()).then(data => console.log(data))
+
